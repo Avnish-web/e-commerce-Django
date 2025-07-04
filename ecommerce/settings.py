1ea855a95a2b09e1a03d76e05ac3657e4c33d433
Avnish <mzamthabhaimzak@gmail.com>
2025-07-04 15:09:03 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')  # Ensure you set this in your environment
+SECRET_KEY = 'SECRET_KEY'  # Ensure you set this in your environment
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # Default to False if not set
+DEBUG = 'True'  # Default to False if not set
 
 ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '127.0.0.1']  # Update with your domain
 
